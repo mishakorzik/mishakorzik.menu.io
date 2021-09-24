d4068b14bc13512a9dfd4419d29a93bc5b607bb6
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-24 22:10:27 +0300

    Update Proxy.py

--- Proxy.py
+++ Proxy.py
@@ -5,33 +5,32 @@ global info
 global proxy
 
 def generateproxy():
-				global proxy
-				global info
+	global proxy
+	global info
 
-				print(Fore.YELLOW+"Genering Proxy Please wait be patient ..."+Style.RESET_ALL)
-				url="https://raw.githubusercontent.com/mishakorzik/mishakorzik.menu.io/master/%D0%A1%D0%B5%D1%80%D0%B2%D0%B5%D1%80/ProxyList.txtU"
-				req = requests.get(url)
-				ip = requests.get("https://raw.githubusercontent.com/mishakorzik/mishakorzik.menu.io/master/docs/ip").text
-				array = req.text.split()
-				open("proxies.txt", "w+").close()
-				for prox in array:
-					      thread_list = []
-					      t = threading.Thread (target=checkproxy, args=(ip, prox))
-					      thread_list.append(t)
-					      t.start()
-				time.sleep(1)
-				f = open("proxies.txt")
-				proxies = f.read().split()
-				proxy = random.choice(proxies)
-				info = Fore.GREEN+"Working proxy successfully found!"+Style.RESET_ALL
+	print(Fore.YELLOW+"Genering Proxy Please wait be patient ..."+Style.RESET_ALL)
+	url="https://raw.githubusercontent.com/mishakorzik/mishakorzik.menu.io/master/%D0%A1%D0%B5%D1%80%D0%B2%D0%B5%D1%80/ProxyList.txtU"
+	req = requests.get(url)
+	ip = requests.get("https://raw.githubusercontent.com/mishakorzik/mishakorzik.menu.io/master/docs/ip").text
+	array = req.text.split()
+	open("proxies.txt", "w+").close()
+	for prox in array:
+		thread_list = []
+		t = threading.Thread (target=checkproxy, args=(ip, prox))
+		thread_list.append(t)
+		t.start()
+	time.sleep(1)
+	f = open("proxies.txt")
+	proxies = f.read().split()
+	proxy = random.choice(proxies)
+	info = Fore.GREEN+"Working proxy successfully found!"+Style.RESET_ALL
 
 def checkproxy(ip, prox):
-				try:
+	try:
                 ipx = requests.get("https://raw.githubusercontent.com/mishakorzik/mishakorzik.menu.io/master/docs/ip", proxies={'http': "http://{}".format(prox), 'https':"http://{}".format(prox)}, verify=False, timeout=10).text
-				except:
-					      ipx = ip
-				if ip != ipx:
-					      f = open("proxies.txt", "a+")
-					      f.write("{}\n".format(prox))
+	except:
+		ipx = ip
+	if ip != ipx:
+		f = open("proxies.txt", "a+")
+		f.write("{}\n".format(prox))
                 f.close()
-					f.close()
