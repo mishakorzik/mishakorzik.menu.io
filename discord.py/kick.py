02f78423c84b222f29f43349e5a2c5595a1287aa
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-04-29 08:54:35 +0300

    Update kick.py

--- discord.py/kick.py
+++ discord.py/kick.py
@@ -3,7 +3,7 @@
 @client.command(name="kick", aliases=['Кикнуть', 'кик', 'Кик', 'кикнуть', 'Kick'])
 @commands.has_permissions(kick_members = True)
 async def kick(ctx, member: discord.Member, *, reason = 'не указано'):
-    title = "👢 • "+member.name+" кикнут(а)."
+    title = member.name+" кикнут(а)."
     async with ctx.typing():
         await member.kick()
         msg = await ctx.send(embed = discord.Embed(colour = discord.Color.red(), description = f"{member} Был кикнутый пользователем, "+ctx.author.name+" по причине: "+reason))
