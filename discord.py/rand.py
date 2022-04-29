9abef82a9b1bb09aaa07e7500b8d7f27b87e4695
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-04-29 08:59:15 +0300

    Create rand.py

---
+++ discord.py/rand.py
@@ -0,0 +1,25 @@
+#Рандомное число от 1 до 100 - random number from 1 to 100
+
+def roll_convert(argument):
+    intarg = int(argument)
+    switcher = {
+        1: ":one:",
+        2: ":two:",
+        3: ":three:",
+        4: ":four:",
+        5: ":five:",
+        6: ":six:",
+        7: ":seven:",
+        8: ":eight:",
+        9: ":nine:",
+        0: ":zero:"
+    }
+    return switcher.get(intarg, "what")
+
+@bot.command(aliases = ['random', 'Random', 'Roll', 'roll', 'Рандом', 'рандом'])
+async def __roll(ctx):
+    r = str(random.randrange(1, 101))
+    mes = 'Рандомное число  -  '
+    for m in r:
+        mes += roll_convert(m)
+    await ctx.send(embed = discord.Embed(colour = discord.Color.gold(), description = mes))
