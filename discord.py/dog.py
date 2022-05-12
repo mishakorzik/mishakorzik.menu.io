5fda6eb31d4effb92506943d1a101920a397ef2f
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-12 09:43:37 +0300

    Create dog.py

---
+++ discord.py/dog.py
@@ -0,0 +1,8 @@
+#Команда которая отправляет рандомную кошку
+#Модули которые нужно скачать: requests, json
+
+@bot.command(aliases=["Кот"])
+async def кот(ctx):
+    get = requests.get("https://some-random-api.ml/animal/dog").json()
+    fox = get['image']
+    await ctx.send(fox)
