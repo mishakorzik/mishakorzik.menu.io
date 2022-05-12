dd52039c5ea3f0dcfe1a4b8792c8519051169dfc
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-12 09:42:35 +0300

    Create cat.py

---
+++ discord.py/cat.py
@@ -0,0 +1,8 @@
+#Команда которая отправляет рандомную кошку
+#Модули которые нужно скачать: requests, json
+
+@bot.command(aliases=["Кот"])
+async def кот(ctx):
+    get = requests.get("https://some-random-api.ml/animal/cat").json()
+    fox = get['image']
+    await ctx.send(fox)
