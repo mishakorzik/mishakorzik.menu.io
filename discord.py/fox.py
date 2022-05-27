12600c4e4f4ab05d11b2dbf554f68be963d8b49f
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-27 21:58:58 +0300

    Update fox.py

--- discord.py/fox.py
+++ discord.py/fox.py
@@ -1,7 +1,7 @@
 #Команда которая отправляет рандомную лису
 #Модули которые нужно скачать: requests, json
 
-@bot.command(aliases=["Лиса"])
+@client.command(aliases=["Лиса"])
 async def лиса(ctx):
     get = requests.get("https://some-random-api.ml/animal/fox").json()
     fox = get['image']
