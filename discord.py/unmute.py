205807c68b900260a0a281a90c51f0dd8ebc13f9
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-27 21:41:23 +0300

    Create unmute.py

---
+++ discord.py/unmute.py
@@ -0,0 +1,8 @@
+@client.command(name="unmute", aliases=['Unmute', 'Розмьт', 'розмьт', 'размьют', 'Размьют'])
+@commands.has_permissions(manage_messages=True)
+async def unmute(ctx, member: discord.Member):
+    mutedRole = discord.utils.get(ctx.guild.roles, name="Зaглушен(а)")
+    await member.remove_roles(mutedRole)
+    await ctx.send(embed = discord.Embed(colour = discord.Color.green(), description = f"{member} Был розмьючен учасником, "+ctx.author.name))
+    title = " 😀 "+member.name+" розмьючен(а)."
+    await member.send(embed = discord.Embed(colour = discord.Color.red(), title = title, description = '> розмьючен на сервере: '+ctx.guild.name+'\n> Администратором: '+ctx.author.name))
