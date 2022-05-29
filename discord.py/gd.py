3be0339fe76eccf60219c05c03eb33f6405b0597
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-29 19:25:23 +0300

    Update gd.py

--- discord.py/gd.py
+++ discord.py/gd.py
@@ -2,6 +2,7 @@
 
 @client.command(name = "geometry", aliases=['Geometry', 'gd', 'Gd', 'геометри', 'Геометри', 'гд', 'Гд'])
 async def geometry(ctx):
+    gd_levels = ["уровень 1", "уровень 2", уровень 3""]
     gd_level = random.choice(gd_levels)
-    title = "⊰🌀⊱ • "+ctx.author.name+" успешно!"
+    title = " 🌀 "+ctx.author.name+" успешно!"
     await ctx.send(embed = discord.Embed(colour = discord.Color.green(), title = title, description = "Успешно! Вот уровень из geometry dash. "+gd_level))
