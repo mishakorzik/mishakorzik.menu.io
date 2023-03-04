7097350cf990811dc257e65fddeb4e23d222c810
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2023-03-04 18:54:02 +0200

    Update he1zen-api-proxy-example.py

--- Сервер/he1zen-api-proxy-example.py
+++ Сервер/he1zen-api-proxy-example.py
@@ -1,27 +1,8 @@
-import requests, json
-
-start = True
-
-    while start:
-
-        try:
-
-            url = "{{settings.apiurl}}/api/proxy/socks5"
-
-            get = requests.get(url).json()
-
-            ip = get["address"]
-
-            port = get["port"]
-
-            proxies = {"http": f"socks5://{ip}:"+str(port)}
-
-            myip = requests.get("https://api.ipify.org", timeout=3, proxies=proxies).text
-
-            print(myip)
-
-            start = False
-
-        except:
-
-            print("proxy not work, finding new proxy.")
+import requests
+print("found 3 working proxy servers")
+for i in range(3):
+   get = requests.get("http://127.0.0.1:8080/api/proxy/socks5").json()
+   ip = str(get["address"])
+   port = str(get["port"])
+   country = str(get["country"])
+   print(f"{ip}:{port} - {country}")
