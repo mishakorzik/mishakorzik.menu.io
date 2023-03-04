7d6a62d2e5937bebabaeff6bbca58162646c32ed
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2023-03-04 19:00:59 +0200

    Delete he1zen-api-proxy-example.py

--- Сервер/he1zen-api-proxy-example.py
+++
@@ -1,8 +0,0 @@
-import requests
-print("found 3 working proxy servers")
-for i in range(3):
-   get = requests.get("http://127.0.0.1:8080/api/proxy/socks5").json()
-   ip = str(get["address"])
-   port = str(get["port"])
-   country = str(get["country"])
-   print(f"{ip}:{port} - {country}")
